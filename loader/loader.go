f486956f0fd66a8ec5af7da1adc49397534e3cb2
Arkadiy Drovosekov <arkdro@gmail.com>
2017-06-23 17:36:34 +0200

    add stub for Load

--- loader/loader.go
+++ loader/loader.go
@@ -1,5 +1,19 @@
 package loader
 
-func Load() {
+func Load(file string, db_data Db_data) {
+	fd, err := xlsx.OpenFile(file)
+	if err != nil {
+	}
+	db, db_err := open_database(db_data)
+	if db_err != nil {
+	}
+	process_data(fd, db)
+}
+
+func open_database(db_data Db_data) (string, error) {
+	return "", nil
+}
+
+func process_data(fd *xlsx.File, db string) {
 }
 
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/asdf/db_load/loader"
+)
+
 func main() {
+	file := ""
+	db_data := loader.Db_data{
+	}
+	loader.Load(file, db_data)
 }
 
