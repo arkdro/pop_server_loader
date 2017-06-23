11728a92e026bd84602028792c8da28e2ff5bdbf
Arkadiy Drovosekov <arkdro@gmail.com>
2017-06-23 19:42:17 +0200

    get command line parameters

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"github.com/asdf/db_load/loader"
+	"flag"
 )
 
 func main() {
-	file := ""
+	var file = flag.String("infile", "", "input file")
+	var hst = flag.String("host", "localhost", "db host")
+	var port = flag.Int("port", 3306, "db port")
+	var user = flag.String("user", "root", "db user")
+	var password = flag.String("password", "root", "db password")
+	var database = flag.String("database", "pdata", "database")
 	db_data := loader.Db_data{
+		Host: *hst,
+		Port: *port,
+		User: *user,
+		Password: *password,
+		Database: *database,
 	}
-	loader.Load(file, db_data)
+	loader.Load(*file, db_data)
 }
 
