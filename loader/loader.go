f9b181a72bc047c76b4f8394774f5c5bfdc4f20e
Arkadiy Drovosekov <arkdro@gmail.com>
2017-06-23 19:42:54 +0200

    exit on input file error

--- loader/loader.go
+++ loader/loader.go
@@ -2,11 +2,13 @@ package loader
 
 import (
 	"github.com/tealeg/xlsx"
+	"log"
 )
 
 func Load(file string, db_data Db_data) {
 	fd, err := xlsx.OpenFile(file)
 	if err != nil {
+		log.Fatal("can not open file: ", err)
 	}
 	db, db_err := open_database(db_data)
 	if db_err != nil {
