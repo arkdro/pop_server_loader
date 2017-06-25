2b515976535973ce4cbc865c7446c15f6887df41
Arkadiy Drovosekov <arkdro@gmail.com>
2017-06-25 14:50:36 +0200

    open mysql connection in a separate function

--- loader/loader.go
+++ loader/loader.go
@@ -1,6 +1,8 @@
 package loader
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/tealeg/xlsx"
 	"fmt"
 	"log"
@@ -11,18 +13,19 @@ func Load(file string, db_data Db_data) {
 	if err != nil {
 		log.Fatal("can not open file: ", err)
 	}
-	db, db_err := open_database(db_data)
-	if db_err != nil {
-		log.Fatal("can not connect to the database: ", db_err)
-	}
+	db := open_database(db_data)
 	process_data(fd, db)
 }
 
-func open_database(db_data Db_data) (string, error) {
-	return "", nil
+func open_database(db_data Db_data) *sql.DB {
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		log.Fatal("can not connect to the database: ", err)
+	}
+	return db
 }
 
-func process_data(fd *xlsx.File, db string) {
+func process_data(fd *xlsx.File, db *sql.DB) {
 	for _, sheet := range fd.Sheets {
 		for _, row := range sheet.Rows {
 			for _, cell := range row.Cells {
